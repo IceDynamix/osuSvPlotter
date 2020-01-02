f02b0fb3cd1e83ecde247e8fc4df50e50227d859
IceDynamix <aerozocker@gmail.com>
2020-01-02 02:14:52 +0100

    Clean up plot elements

--- lib/osuPlotElements.R
+++ lib/osuPlotElements.R
@@ -1,29 +1,33 @@
 source("lib/osu.R")
 
+
 geom_osuBookmarks <- function(osu) {
   plotElement <- geom_vline(
-        color = "dodgerblue",
-        xintercept = osu.getBookmarks(osu),
-        alpha = 0.5
-    )
+    color = "dodgerblue",
+    xintercept = osu.getBookmarks(osu),
+    alpha = 0.5
+  )
+
   return(plotElement)
 }
+
 
 geom_osuBpmPoints <- function(osu) {
   svPoints <- osu.getBpmPoints(osu)
   plotElement <- geom_vline(
-        color = "red",
-        xintercept = svPoints$time,
-        alpha = 0.15
-    )
+    color = "red",
+    xintercept = svPoints$time,
+    alpha = 0.15
+  )
   return(plotElement)
 }
+
 
 geom_osuKiais <- function(osu) {
 
   svPoints <- osu.getSvPoints(osu)
 
-  # grabs all kiais and orders them into start, end
+  # grabs all kiais and orders them into a data frame with start, end
   kiaiSwitches <- osu$timingPoints %>%
     as_tibble() %>%
     filter(effects != lag(effects)) %>%
@@ -38,28 +42,31 @@ geom_osuKiais <- function(osu) {
 
   colnames(kiaiSwitches) <- c("x1", "x2")
 
-  plotElement <- geom_rect(data = kiaiSwitches,
-        mapping = aes(
-            xmin = x1,
-            xmax = x2,
-            ymin = 0,
-            ymax = max(osu$timingPoints["value"])
-        ),
-        alpha = 0.1,
-        fill = "orange",
-        inherit.aes = FALSE
-    )
+  # The kiai highlights on the output graph are nothing but rectangles
+  plotElement <- geom_rect(
+    data = kiaiSwitches,
+    mapping = aes(
+      xmin = x1,
+      xmax = x2,
+      ymin = 0,
+      ymax = max(10e10) # Using pseudo-infinity
+    ),
+    alpha = 0.1,
+    fill = "orange",
+    inherit.aes = FALSE
+  )
 
   return(plotElement)
 }
 
+
 geom_osuSvRollingAvg <- function(osu) {
   svPoints <- osu.getSvPoints(osu)
   plotElement <- geom_line(
-      data = osu.getSvRollingAverage(svPoints),
-      mapping = aes(time, rollingAvg),
-      color = "orange"
-    )
+    data = osu.getSvRollingAverage(svPoints),
+    mapping = aes(time, rollingAvg),
+    color = "orange"
+  )
 
   return(plotElement)
 }
